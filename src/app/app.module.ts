c95d6cd949c3085674eaf3060352fe0eefa7b137
AriadnaLoboFS <ariadnalobo1@gmail.com>
2022-09-19 22:42:04 -0300

    tercer git - se agregaron todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HeaderComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    FooterComponent,
+    SobremiComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
