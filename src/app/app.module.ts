e833e2ba06fc9a4c208467b5dcde235a22d80f22
AriadnaLoboFS <ariadnalobo1@gmail.com>
2022-10-02 02:03:36 -0300

    septimo commit intento de conectar el front con el back fail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SkillsComponent } from './componentes/skills/skills.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { SobremiComponent } from './componentes/sobremi/sobremi.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import {HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
